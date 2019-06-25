18f2387d53e1d69704cfb070dd7aa514824634ec
Sung Lee <sungsoolee2@hotmail.com>
2019-06-24 20:34:21 -0500

    changes added

--- assets/liri.js
+++ assets/liri.js
@@ -1,7 +1,14 @@
-//access key info through var spotify//
+//access spotify key through .env file//
 require("dotenv").config();
-var keys = require("./assets/keys.js");
+// console.log(require("dotenv").config())
+
+//set spotify key and assign node spotify api//
+var keys = require("./keys.js");
+// console.log(keys)
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
+// src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"
 
 // load the fs package to read and write//
 var fs = require("fs");
@@ -33,25 +40,28 @@ switch (action) {
         break;
 }
 function movie() {
+    // default name of movie is Mr. Nobody//
+    if (name == "undefined") { name = "Mr. Nobody" }
 
-
+    console.log(name);
     var link = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy";
 
-    // default name of movie is Mr. Nobody//
 
     // Then run a request with axios to the OMDB API with the movie specified
-    axios.get(link).then(
-        function (response) {
-            console.log("The movie's title is: " + response.data.Title);
-            console.log("The movie came out in : " + response.data.Year);
-            console.log("The movie's rating is: " + response.data.imdbRating);
-            console.log("The movies's Rotton Tomatoes rating is : " + response.data.Ratings[1].Value);
-            console.log("The movie was produced in this country(s) : " + response.data.Country);
-            console.log("The movie's language is: " + response.data.Language);
-            console.log("The movie's plot is: " + response.data.Plot);
-            console.log("The actors in the movie are: " + response.data.Actors);
+    axios.get(link)
 
-        })
+        .then(
+            function (response) {
+                console.log("The movie's title is: " + response.data.Title);
+                console.log("The movie came out in : " + response.data.Year);
+                console.log("The movie's rating is: " + response.data.imdbRating);
+                console.log("The movies's Rotton Tomatoes rating is : " + response.data.Ratings[1].Value);
+                console.log("The movie was produced in this country(s) : " + response.data.Country);
+                console.log("The movie's language is: " + response.data.Language);
+                console.log("The movie's plot is: " + response.data.Plot);
+                console.log("The actors in the movie are: " + response.data.Actors);
+
+            })
 
         .catch(function (error) {
             if (error.response) {
@@ -83,15 +93,17 @@ function concert() {
     axios.get(link).then(
         function (response) {
 
-            console.log(response.upcoming_event_count)
-            var counter = parseInt(response.upcoming_event_count);
-            
             // for loop to loop through all the events and display info for each event//
-
+            // console.log(response.data);
             for (var i in response.data) {
-                console.log("Name of venue:" + response.data[i].venue);
-                console.log("Venue location: " + response.data[i].city);
-                console.log("Date of Event: " + moment(response.data[i].datetime).format(MM/DD/YYYY)
+                var namevenue= response.data[i].venue.name;
+                var locvenue= response.data[i].venue.city;
+                var dateevent= moment(response.data[i].datetime).format('LLL');
+                console.log("Name of venue: " + namevenue + "  Venue location:  " + locvenue + "  Date of event:  " + dateevent)
+                // console.log("Name of venue:" + response.data[i].venue.name);
+                // console.log("Venue location: " + response.data[i].venue.city);
+                // console.log("Date of Event: " + response.data[i].datetime);
+                // console.log("Date of Event: " + moment(response.data[i].datetime).format('LLL'));
             }
         })
 
@@ -119,20 +131,26 @@ function concert() {
 
 function spotifyit() {
 
-    var link = "https://www.npmjs.com/package/node-spotify-api"
 
-    //default song name is The Sign from Ace of Base//
+// default song name is The Sign from Ace of Base//
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
 
-    // Then run a request with axios to the Spotify API with the song specified
-    axios.get(link).then(
-        function (response) {
-            console.log("The Artist is: " + response);
-            console.log("The Song Name is : " + response);
-            console.log("Preview link URL " + response);
-            console.log("The Album that the song is from is : " + response);
+//   console.log(data); 
+//   });
+// Then run a request with spotify to the Spotify API with the song specified
+spotify.search({ type: "track", query: name }).then(function (response) {
+    console.log("The Artist is: " + response.tracks.next);
+    console.log("The Artist is: " + response.tracks.href);
+    console.log("The Artist is: " + response.tracks);
+    console.log("The Song Name is : " + response.tracks.items);
+    console.log("Preview link URL " + response.tracks);
+    console.log("The Album that the song is from is : " + response);
 
 
-        })
+})
 
         .catch(function (error) {
             if (error.response) {
@@ -164,13 +182,15 @@ function dowhat() {
         }
         //store random.text  data into var outut split by the ","//
         var output = data.split(",");
-        console.log(output[0]);
-        console.log(output[1]);
+        // console.log(output[0]);
+        // console.log(output[1]);
 
-        //assign reasd text into action and name var //
-        var action = output[0];
-        var name = output[1];
-
+        //assign read text into action and name var  //
+        action = output[0];
+        name = output[1];
+        // console.log(action);
+        // console.log(name);
+       
         switch (action) {
             case "spotify-this-song":
                 spotifyit(name);
